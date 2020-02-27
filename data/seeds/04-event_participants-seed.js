18ad10d2a1246a62bdc01703ab3edfd09f91e5a7
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-27 09:49:45 +0000

    Add event_pariciapants and Event_team seeds

--- data/seeds/04-event_participants-seed.js
+++ data/seeds/04-event_participants-seed.js
@@ -1,13 +1,19 @@
+/* eslint-disable func-names */
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+
+exports.seed = function (knex) {
+  return knex('tevent_participants').truncate()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('event_participants').insert([
+        { "event_id": 1, "user_id": 1 },
+        { "event_id": 1, "user_id": 2 },
+        { "event_id": 1, "user_id": 3 },
+        { "event_id": 2, "user_id": 4 },
+        { "event_id": 2, "user_id": 5 },
+        { "event_id": 2, "user_id": 6 },
+        { "event_id": 3, "user_id": 3 },
+        { "event_id": 3, "user_id": 4 },
+        { "event_id": 3, "user_id": 5 }
       ]);
     });
 };
--- data/seeds/05-event_team-seed.js
+++ data/seeds/05-event_team-seed.js
@@ -1,13 +1,16 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('event_team').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('event_team').insert([
+        { "event_id": 1, "user_id": 1, "role_type": "organizer" },
+        { "event_id": 2, "user_id": 3, "role_type": "organizer" },
+        { "event_id": 3, "user_id": 2, "role_type": "organizer" },
+        { "event_id": 1, "user_id": 6, "role_type": "judge" },
+        { "event_id": 2, "user_id": 5, "role_type": "judge" },
+        { "event_id": 3, "user_id": 4, "role_type": "judge" }
       ]);
     });
 };
