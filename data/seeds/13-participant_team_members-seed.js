0ee993c2a54a7f38303bf5e4bb9c0222087c2807
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-27 10:52:41 +0000

    Add participant_team_members seed

--- data/seeds/13-participant_team_members-seed.js
+++ data/seeds/13-participant_team_members-seed.js
@@ -1,13 +1,46 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('participant_team_members').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('participant_team_members').insert([
+        {
+          "team_member": 3,
+          "team_id": 1
+        },
+        {
+          "team_member": 4,
+          "team_id": 2
+        },
+        {
+          "team_member": 5,
+          "team_id": 3
+        },
+        {
+          "team_member": 6,
+          "team_id": 4
+        },
+        {
+          "team_member": 1,
+          "team_id": 5
+        },
+        {
+          "team_member": 2,
+          "team_id": 6
+        },
+        {
+          "team_member": 3,
+          "team_id": 1
+        },
+        {
+          "team_member": 4,
+          "team_id": 2
+        },
+        {
+          "team_member": 5,
+          "team_id": 3
+        }
       ]);
     });
 };
