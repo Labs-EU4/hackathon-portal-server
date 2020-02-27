4d6f22bb5074cabd256b5cd4969a0c99a5f7bcd8
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-02-27 10:32:27 +0000

    Add project_grading seed

--- data/seeds/09-project_grading-seed.js
+++ data/seeds/09-project_grading-seed.js
@@ -1,13 +1,40 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('project_grading').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('project_grading').insert([
+        {
+          "project_event_id": 1,
+          "project_id": 1,
+          "judge_id": 6
+        },
+        {
+          "project_event_id": 2,
+          "project_id": 2,
+          "judge_id": 5
+        },
+        {
+          "project_event_id": 3,
+          "project_id": 3,
+          "judge_id": 4
+        },
+        {
+          "project_event_id": 2,
+          "project_id": 4,
+          "judge_id": 5
+        },
+        {
+          "project_event_id": 2,
+          "project_id": 5,
+          "judge_id": 5
+        },
+        {
+          "project_event_id": 1,
+          "project_id": 6,
+          "judge_id": 6
+        }
       ]);
     });
 };
