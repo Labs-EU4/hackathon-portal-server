a5a4a80f324984195e3d917479b469ecb3958bf9
Emma Andrews <54676884+ELAndrews@users.noreply.github.com>
2020-03-05 14:33:50 +0000

    Update knexfile.js
    Amend temp change back to original

--- knexfile.js
+++ knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   test: {
     client: 'pg',
-    connection: process.env.HEROKU_POSTGRESQL_CRIMSON_URL,
+    connection: process.env.DATABASE_TEST_URL,
     migrations: {
       directory: './data/migrations'
     },
